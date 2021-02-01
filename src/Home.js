3533c1e18c91e3cf5101ebbd6a14f04a161f5def
mrautenberg <max.rautenberg89@gmail.com>
2021-02-01 15:27:05 +0100

    :sparkles: react-router and custom hook  added

--- src/Home.js
+++ src/Home.js
@@ -1,31 +1,8 @@
-import { useState, useEffect } from "react"
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [isPending, setIsPending] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    setTimeout(() => {
-      fetch("http://localhost:8000/blogs")
-        .then(res => {
-          if (!res.ok) {
-            throw Error("could not fetch the data for that resource")
-          }
-          return res.json();
-        })
-        .then(data => {
-          setBlogs(data);
-          setIsPending(false);
-          setError(null);
-        })
-        .catch(err => {
-          setIsPending(false);
-          setError(err.message);
-        })
-    }, 1000);
-  }, []);
+  const { data: blogs, isPending, error } = useFetch("http://localhost:8000/blogs");
 
   return (
     <div className="home">
@@ -33,7 +10,7 @@ const Home = () => {
       {isPending && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} title="All Blogs" />}
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,15 +1,17 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
   return (
     <div>
       <nav className="navbar">
         <h1>The Dojo Blog</h1>
         <div className="links">
-          <a href="/">Home</a>
-          <a href="/create">New Blog</a>
+          <Link to="/">Home</Link>
+          <Link to="/create">New Blog</Link>
         </div>
       </nav>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
